180884e7ab73d63f750367bef03b8851de458bf9
Natalia Shnur <nataliashnur@gmail.com>
2023-10-26 09:31:04 -0500

    GET/validate/username/available/{username} endpoint created and tested

--- socialmedia/src/main/java/com/cooksys/socialmedia/controllers/ValidateController.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/controllers/ValidateController.java
@@ -1,7 +1,7 @@
 package com.cooksys.socialmedia.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cooksys.socialmedia.services.ValidateService;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +9,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/validate")
 public class ValidateController {
+
+    private final ValidateService validateService;
+
+    @GetMapping("/username/available/{username}")
+    public boolean isUsernameAvailable(@PathVariable String username) {
+        return validateService.isUsernameAvailable(username);
+    }
+
 }
--- socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
@@ -1,9 +1,17 @@
 package com.cooksys.socialmedia.services.impl;
 
+import com.cooksys.socialmedia.repositories.UserRepository;
+import com.cooksys.socialmedia.services.ValidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ValidateServiceImpl {
+public class ValidateServiceImpl implements ValidateService {
+
+    private final UserRepository userRepository;
+    @Override
+    public boolean isUsernameAvailable(String username) {
+        return !userRepository.existsByCredentialsUsername(username);
+    }
 }
