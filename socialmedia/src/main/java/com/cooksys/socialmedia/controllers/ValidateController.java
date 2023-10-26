e124ce44fd0072b176c2f1ca0e3a2087dd498570
Ro Kim <rookim4@gmail.com>
2023-10-26 10:56:40 -0500

    adds validate/tag/exists/{label} endpoint

--- socialmedia/src/main/java/com/cooksys/socialmedia/controllers/ValidateController.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/controllers/ValidateController.java
@@ -1,7 +1,11 @@
 package com.cooksys.socialmedia.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.socialmedia.services.ValidateService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +13,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/validate")
 public class ValidateController {
+	private final ValidateService validateService;
+	
+	@GetMapping("/tag/exists/{label}")
+	public boolean tagExists(@PathVariable String label) {
+		return validateService.tagExists(label);
+	}
 }
