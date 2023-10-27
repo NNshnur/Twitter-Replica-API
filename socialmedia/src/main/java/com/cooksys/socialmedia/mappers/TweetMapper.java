b9f4236e61108335a663a47704319c55dff264a5
Natalia Shnur <nataliashnur@gmail.com>
2023-10-26 19:02:16 -0500

    Tweet endpoint: post_/{id}/reply implemented and tested

--- socialmedia/src/main/java/com/cooksys/socialmedia/mappers/TweetMapper.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/mappers/TweetMapper.java
@@ -1,7 +1,14 @@
 package com.cooksys.socialmedia.mappers;
 
+import com.cooksys.socialmedia.dto.TweetResponseDto;
+import com.cooksys.socialmedia.entities.Tweet;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel="spring", uses = {UserMapper.class})
 public interface TweetMapper {
+
+   TweetResponseDto entityToResponseDto(Tweet tweet);
+
 }
--- socialmedia/src/main/java/com/cooksys/socialmedia/services/TweetService.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/TweetService.java
@@ -1,4 +1,15 @@
 package com.cooksys.socialmedia.services;
 
+import com.cooksys.socialmedia.dto.TweetRequestDto;
+import com.cooksys.socialmedia.dto.TweetResponseDto;
+import com.cooksys.socialmedia.dto.UserResponseDto;
+
+import java.util.List;
+
 public interface TweetService {
+    List<TweetResponseDto> getRepostsOfTweet(Long repostOf);
+
+    List<UserResponseDto> getUsersMentioned(Long id);
+
+    TweetResponseDto createTweetReply(Long id, TweetRequestDto tweetRequestDto);
 }
